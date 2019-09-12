9809147a6a23b7fe6ed527f7f36d8012249dd44f
JoRPM <44869196+JoRPM@users.noreply.github.com>
2019-09-12 11:49:44 -0300

    firebase
    tentando implementar o firebase

--- appesta/src/app/app.module.ts
+++ appesta/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,12 +20,21 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({apiKey: "AIzaSyDut3PWbHMlWwxJt-hQRgJb09bH8oYtxp0",
+    authDomain: "appesta-99f09.firebaseapp.com",
+    databaseURL: "https://appesta-99f09.firebaseio.com",
+    projectId: "appesta-99f09",
+    storageBucket: "appesta-99f09.appspot.com",
+    messagingSenderId: "878145172937",
+    appId: "1:878145172937:web:27d80f8c3b9a795b0c6418"}),
+    AngularFireDatabaseModule
+  
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    Geolocation,
+
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
